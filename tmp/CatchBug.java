803165123a2c2eeb1e153421c3eb765bb7574725
sunxiaoou <sun_xiaoou@qq.com>
2017-08-12 17:04:38 +0800

    AbnormalDead

--- tmp/CatchBug.java
+++ tmp/CatchBug.java
@@ -2,12 +2,16 @@
 // Enums using "tables" instead of multiple dispatch.
 package tmp;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Random;
+
 
 public class CatchBug {
   private static Random rand = new Random(47);
   private static Monkey monkeys[];
   private static Monkey work[];
+  //  private static int dead = 0;
 
   enum Roll {
     GOOD, MEDIUM, BAD;
@@ -21,20 +25,37 @@ public class CatchBug {
             { {2, 2}, {2, 2}, {0, 1} }, // MEDIUM
             { {2, 0}, {0, 0}, {0, 0} } // BAD
     };
-    public int[] compete(Roll other) {
-      return table[this.ordinal()][other.ordinal()];
-    }
 
-    public Roll reborn() {
+    private static int [] quantities = new int[3];
+
+    int[] compete(Roll other) { return table[ordinal()][other.ordinal()]; }
+
+    static void clean() { Arrays.fill(quantities, 0); }
+    static int quantity(Roll roll) { return quantities[roll.ordinal()]; }
+    static boolean isExtinct() {
+      for (int i = 0; i < quantities.length; i ++)
+        if (quantities[i] == 0)
+          return true;
+      return false;
+    }
+    void born() { quantities[ordinal()] ++; }
+
+    Roll reborn() {
+      Roll other = null;
+      quantities[ordinal()] --;
       switch (this) {
         case GOOD:
-          return rand.nextInt(2) == 0 ? MEDIUM : BAD;
+          other = rand.nextInt(2) == 0 ? MEDIUM : BAD;
+          break;
         case MEDIUM:
-          return rand.nextInt(2) == 0 ? BAD : GOOD;
+          other = rand.nextInt(2) == 0 ? BAD : GOOD;
+          break;
         case BAD:
-          return rand.nextInt(2) == 0 ? GOOD : MEDIUM;
+          other = rand.nextInt(2) == 0 ? GOOD : MEDIUM;
+          break;
       }
-      return null;
+      quantities[other.ordinal()] ++;
+      return other;
     }
   }
 
@@ -44,9 +65,14 @@ public class CatchBug {
     private Roll roll;
     private static final int increment = 2;
     private static final int max_bugs = 10;
+    private static int deadQuantity = 0;
     private int value = 0;    // current number of bugs
 
-    Monkey(Roll roll) { this.roll = roll; }
+    Monkey(Roll roll) {
+      roll.born();
+      this.roll = roll;
+    }
+
     Roll getRoll() { return roll; }
     // void setRoll(Roll roll) { this.roll = roll; }
     // int getValue() { return value; }
@@ -66,34 +92,39 @@ public class CatchBug {
       if (value > max_bugs) {     // died and reborn
         value = 0;
         roll = roll.reborn();
+        deadQuantity ++;
       }
     }
+
+    static void clean() { deadQuantity = 0; }
+    static int deadQuantity() { return deadQuantity; }
 
     public String toString() { return String.format("%03d_%s(%d)", id, roll, value); }
   }
 
-  private static void print() {
+  private static void print(String good) {
     for (Monkey monkey : monkeys)
       System.out.print(monkey + ", ");
     System.out.println();
   }
 
+  private static String track(String roll, int quantity) {
+    StringBuilder s = new StringBuilder(roll);
+    int number = roll.equals("AbnormalDead") ? quantity / 100 : quantity / 10;
+    for(int i = 0; i < number; i ++)
+      s.append("*");
+    s.append("----").append(quantity);
+    if (! roll.equals("AbnormalDead") && quantity == 0)
+      s.append(" (is extinct)");
+
+    return s.toString();
+  }
+
   private static void count() {
-    int nGood = 0, nMedium = 0, nBad = 0;
-    for (Monkey monkey : monkeys) {
-      switch (monkey.getRoll()) {
-        case GOOD:
-          nGood ++;
-          break;
-        case MEDIUM:
-          nMedium ++;
-          break;
-        case BAD:
-          nBad ++;
-          break;
-      }
-    }
-    System.out.printf("GOOD(%d)MEDIUM(%d)BAD(%d)\n", nGood, nMedium, nBad);
+    System.out.println(track("Good", Roll.quantity(Roll.GOOD)));
+    System.out.println(track("Medium", Roll.quantity(Roll.MEDIUM)));
+    System.out.println(track("Bad", Roll.quantity(Roll.BAD)));
+    System.out.println(track("AbnormalDead", Monkey.deadQuantity()));
   }
 
   private static void shuttle() {
@@ -105,23 +136,9 @@ public class CatchBug {
     }
   }
 
-  private static void oneRound() {
-    System.arraycopy(monkeys, 0, work, 0, monkeys.length);
-    shuttle();
-    // print();
-
-    for (int i = 0; i < monkeys.length; i += 2) {
-      Monkey a = work[i];
-      Monkey b = work[i + 1];
-      a.compete(b);
-      // System.out.printf("%s vs. %s\n", a, b);
-    }
-
-    for (Monkey monkey : work)
-      monkey.reborn();
-  }
-
-  private CatchBug(int nGood, int nMedium, int nBad, int nRound) {
+  private CatchBug(int nGood, int nMedium, int nBad, int nRound) throws IOException {
+    Roll.clean();
+    Monkey.clean();
     if ((nGood + nMedium + nBad) % 2 != 0)
       nGood ++;
     monkeys = new Monkey[nGood + nMedium + nBad];
@@ -134,16 +151,49 @@ public class CatchBug {
     for (; i < nGood + nMedium + nBad; i ++)
       monkeys[i] = new Monkey(Roll.BAD);
 
+    // System.out.printf("\n\nGood(%d)Medium(%d)Bad(%d)\n", nGood, nMedium, nBad);
+    System.out.printf("\n\nTest begins ...\n");
+    count();
     for (i = 0; i < nRound; i ++) {
-      oneRound();
-      if (i % 100 == 0) {
-        System.out.printf("%03d: ", i);
-        count();
+      System.arraycopy(monkeys, 0, work, 0, monkeys.length);
+      shuttle();
+      // print();
+
+      for (int j = 0; j < monkeys.length; j += 2) {
+        Monkey a = work[j];
+        Monkey b = work[j + 1];
+        a.compete(b);
+        // System.out.printf("%s vs. %s\n", a, b);
       }
+
+      for (Monkey monkey : work)
+        monkey.reborn();
+
+      /*
+      if (i % 10 == 0) {
+        System.out.printf("%03d: \n", i);
+        count();
+        try {
+          Thread.sleep(200);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+      */
+
+      if (Roll.isExtinct())
+        break;
     }
+    System.out.printf("\nTest ends at Round %03d:\n", i);
+    count();
+    // System.out.println("Input any key to continue ...");
+    // System.in.read();
   }
 
-  public static void main(String[] args) {
-    new CatchBug(1000, 100, 100, 100000);
+  public static void main(String[] args) throws Exception {
+    new CatchBug(400, 400, 400, 200);
+    new CatchBug(1000, 100, 100, 200);
+    new CatchBug(100, 1000, 100, 200);
+    new CatchBug(100, 100, 1000, 200);
   }
 }
