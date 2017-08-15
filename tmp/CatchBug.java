5efab3123af28203100923258852e089c972dfbf
sunxiaoou <sun_xiaoou@qq.com>
2017-08-15 21:40:05 +0800

    change output

--- tmp/CatchBug.java
+++ tmp/CatchBug.java
@@ -2,7 +2,6 @@
 // Enums using "tables" instead of multiple dispatch.
 package tmp;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -13,7 +12,16 @@ public class CatchBug {
   private static Random rand = new Random(47);
   private static Monkey monkeys[];
   private static Monkey work[];
-  //  private static int dead = 0;
+
+  private static int fieldWidth = 10;
+  private static String stringField() { return "%" + fieldWidth + "s"; }
+  private static String numberField() {
+    return "%" + fieldWidth + "d";
+  }
+  private static int sizeWidth = 6;
+  private static String sizeField = "%" + sizeWidth + "s";
+  private String headline = "";
+
 
   enum Roll {
     GOOD, MEDIUM, BAD;
@@ -76,9 +84,6 @@ public class CatchBug {
     }
 
     Roll getRoll() { return roll; }
-    // void setRoll(Roll roll) { this.roll = roll; }
-    // int getValue() { return value; }
-    // void setValue(int value) { this.value = value; }
     void increase() { value += increment; }
     void decrease(int a) { value = value > a ? value - a : 0; }
 
@@ -104,29 +109,35 @@ public class CatchBug {
     public String toString() { return String.format("%03d_%s(%d)", id, roll, value); }
   }
 
-  private static void print(String good) {
-    for (Monkey monkey : monkeys)
-      System.out.print(monkey + ", ");
+  private void displayHeader() {
+    // Calculate width and pad with '-':
+    int width = fieldWidth * (Roll.values().length + 1) + sizeWidth;
+    int dashLength = width - headline.length() - 1;
+    StringBuilder head = new StringBuilder(width);
+    for(int i = 0; i < dashLength / 2; i++)
+      head.append('-');
+    head.append(' ');
+    head.append(headline);
+    head.append(' ');
+    for(int i = 0; i < dashLength/2; i++)
+      head.append('-');
+    System.out.println(head);
+    // Print column headers:
+    System.out.format(sizeField, "Round#");
+    for(Roll roll : Roll.values())
+      System.out.format(stringField(), roll);
+    System.out.format(stringField(), "Killed");
+
     System.out.println();
   }
 
-  private static String track(String roll, int quantity) {
-    StringBuilder s = new StringBuilder(roll);
-    int number = roll.equals("AbnormalDead") ? quantity / 100 : quantity / 10;
-    for(int i = 0; i < number; i ++)
-      s.append("*");
-    s.append("----").append(quantity);
-    if (! roll.equals("AbnormalDead") && quantity == 0)
-      s.append(" (is extinct)");
+  private static void print(int round) {
+    System.out.format(sizeField, round);
+    for(Roll roll : Roll.values())
+      System.out.format(numberField(), Roll.quantity(roll));
+    System.out.format(numberField(), Monkey.deadQuantity());
 
-    return s.toString();
-  }
-
-  private static void count() {
-    System.out.println(track("  Good", Roll.quantity(Roll.GOOD)));
-    System.out.println(track("  Medium", Roll.quantity(Roll.MEDIUM)));
-    System.out.println(track("  Bad", Roll.quantity(Roll.BAD)));
-    System.out.println(track("  AbnormalDead", Monkey.deadQuantity()));
+    System.out.println();
   }
 
   private static void shuttle() {
@@ -138,7 +149,8 @@ public class CatchBug {
     }
   }
 
-  private CatchBug(int nGood, int nMedium, int nBad, int nRound) throws IOException {
+  private CatchBug(int nGood, int nMedium, int nBad, int nRound) {
+    headline = String.format("Test%02d", id);
     Roll.clean();
     Monkey.clean();
     if ((nGood + nMedium + nBad) % 2 != 0)
@@ -153,13 +165,11 @@ public class CatchBug {
     for (; i < nGood + nMedium + nBad; i ++)
       monkeys[i] = new Monkey(Roll.BAD);
 
-    // System.out.printf("\n\nGood(%d)Medium(%d)Bad(%d)\n", nGood, nMedium, nBad);
-    System.out.printf("\nTest%02d begins ...\n", id);
-    count();
+    displayHeader();
+    print(0);
     for (i = 0; i < nRound; i ++) {
       System.arraycopy(monkeys, 0, work, 0, monkeys.length);
       shuttle();
-      // print();
 
       for (int j = 0; j < monkeys.length; j += 2) {
         Monkey a = work[j];
@@ -171,28 +181,13 @@ public class CatchBug {
       for (Monkey monkey : work)
         monkey.reborn();
 
-      /*
-      if (i % 10 == 0) {
-        System.out.printf("%03d: \n", i);
-        count();
-        try {
-          Thread.sleep(200);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-      }
-      */
-
       if (Roll.isExtinct())
         break;
     }
-    System.out.printf("Test%02d ends at Round %03d:\n", id, i);
-    count();
-    // System.out.println("Input any key to continue ...");
-    // System.in.read();
+    print(i + 1);
   }
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     new CatchBug(400, 400, 400, 200);
     new CatchBug(1000, 100, 100, 200);
     new CatchBug(100, 1000, 100, 200);
